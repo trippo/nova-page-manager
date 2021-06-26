d575741b40af683cdc25d19cdc11121cdae12fe4
Alberto Peripolli <info@albertoperipolli.com>
2021-06-26 15:11:55 +0200

    - migration refactoring

--- database/migrations/2019_03_13_000000_create_pages_table.php
+++ database/migrations/2019_03_13_000000_create_pages_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use OptimistDigital\NovaPageManager\NovaPageManager;
 
-class CreatePagesTables extends Migration
+class CreatePagesTable extends Migration
 {
     /**
      * Run the migrations.
